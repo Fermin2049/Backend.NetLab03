ef82ebabf5501cd4bca0102914de20092f3a33d7
Fermin2049 <fermin2049@gmail.com>
2024-11-14 04:33:48 -0300

    hacualisando la base de datos

---
+++ Migrations/20241114072206_AddPropietarioIdToInquilino.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TpFinalLaboratorio.Net.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropietarioIdToInquilino : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PropietarioId",
+                table: "Inquilinos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inquilinos_PropietarioId",
+                table: "Inquilinos",
+                column: "PropietarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inquilinos_Propietarios_PropietarioId",
+                table: "Inquilinos",
+                column: "PropietarioId",
+                principalTable: "Propietarios",
+                principalColumn: "IdPropietario",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inquilinos_Propietarios_PropietarioId",
+                table: "Inquilinos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inquilinos_PropietarioId",
+                table: "Inquilinos");
+
+            migrationBuilder.DropColumn(
+                name: "PropietarioId",
+                table: "Inquilinos");
+        }
+    }
+}
